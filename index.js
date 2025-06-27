e782414e585fdebb4fa8b69a97329856d219273e
leslie <leslie.ojuma@student.moringaschool.com>
2025-06-27 18:09:11 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seen = new Set();
+  for (let num of array) {
+    const complement = target - num;
+    if (seen.has(complement)) return true;
+    seen.add(num);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n) - we traverse the array once and each lookup in the set is O(1)
 */
 
 /* 
   Add your pseudocode here
+  create a set to keep track of numbers we've seen
+  for each number in the array
+    calculate the complement
+    if (complement in seen) return true;
+    seen.add(num);
+  }
+  return false;
 */
 
 /*
   Add written explanation of your solution here
+  We use a set to keep track of the numbers we've seen so far.
+  For each number in the array, we calculate its complement (the number we need to find).
+  If the complement is already in the set, we return true.
+  If not, we add the current number to the set and continue.
+  If we finish the loop without finding a pair, we return false.
 */
 
 // You can run `node index.js` to view these console logs
